6cba01343784aadbba2e88e35499d18aad8c5934
Hayden <asianhaydenxd@gmail.com>
2022-06-16 01:39:45 -0400

    Declare basic asceng structs

--- src/asceng.h
+++ src/asceng.h
@@ -3,4 +3,22 @@
 #ifndef ASCIIENGINE_H
 #define ASCIIENGINE_H
 
+#include <stddef.h>
+
+struct Entity
+{
+    char name[20];
+    size_t id;
+};
+
+struct Scene
+{
+    struct Entity entities[256];
+};
+
+struct Project
+{
+    struct Scene scenes[16];
+};
+
 #endif
